12e2579d6ce21c9119a2d2667993043e921de604
Riadhsiki <riadhtr21@gmail.com>
2025-05-04 03:24:55 +0100

    Update .gitignore and MainFX.java

--- src/main/java/Main/MainFX.java
+++ src/main/java/Main/MainFX.java
@@ -2,27 +2,22 @@ package Main;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import javafx.scene.image.Image;
 
+import java.io.IOException;
 
 public class MainFX extends Application {
-
     @Override
-    public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/Reclamation/ReclamationList.fxml"));
-        Scene scene = new Scene(root, 900, 600);
-
-
-
-        primaryStage.setTitle("Cultify");
-        primaryStage.setScene(scene);
-        primaryStage.show();
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(MainFX.class.getResource("/userinterfaces/Home.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 800, 600);
+        stage.setTitle("Cultify");
+        stage.setScene(scene);
+        stage.show();
     }
 
     public static void main(String[] args) {
-        launch(args);
+        launch();
     }
 }
